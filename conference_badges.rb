8984755f1bd80fbe2714653fec9eb51a14eb73e9
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-09 03:22:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def batch_badge_creator(array)
   for i in 0..array.length-1
-    array[i] = "Hello, my name is #{array[i]}"
+    array[i] = "Hello, my name is #{array[i]}."
   end
   return array
 end
